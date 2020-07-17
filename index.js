b93907e8c356f1ed3c466c8f65b4c20aa0242694
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-17 15:55:48 -0700

    added my most commonly used array methods

---
+++ index.js
@@ -0,0 +1,26 @@
+// ================ARRAYS================
+// slice       // Extracts a section of an array and returns a new array.
+// splice        // Adds and/or removes elements from an array.
+// find          // Returns the found value in the array, if an element in the array satisfies the provided testing function or undefined if not found.
+// concat        // Returns a new array comprised of this array joined with other array(s) and/or value(s).
+// join          // Joins all elements of an array into a string.
+// filter        // Creates a new array with all of the elements of this array for which the provided filtering function returns true.
+// map           // Creates a new array with the results of calling a provided function on every element in this array.
+// reduce        // Apply a function against an accumulator and each value of the array (from left-to-right) as to reduce it to a single value.
+// reverse       // Reverses the order of the elements of an array in place — the first becomes the last, and the last becomes the first.
+// sort          // Sorts the elements of an array in place and returns the array.
+// push          // Adds one or more elements to the end of an array and returns the new length of the array.
+// indexOf       // Returns the first (least) index of an element within the array equal to the specified value, or -1 if none is found.
+// includes      // Determines whether an array contains a certain element, returning true or false as appropriate.
+// lastIndexOf   // Returns the last (greatest) index of an element within the array equal to the specified value, or -1 if none is found.
+// 
+
+
+
+
+// ================Objects================
+// 
+
+
+// ================Strings================
+// split
